63630c7cb44b656e450192de445612c648897700
Hundo1018 <f74169@gmail.com>
2021-05-21 11:59:37 +0800

    hot fix，修正因TownCenter無法執行的問題

---
+++ Source/Units/TownCenter.cpp
@@ -0,0 +1,34 @@
+#include "../World.h"
+
+void Unit::TownCenter::SetBitmap()
+{
+	string str = "..\\RES\\Building\\TownCenter.bmp";
+	animations[State::Idle][Direction::Down].AddBitmap(const_cast<char*>(str.c_str()), RGB(0, 0, 0));
+	size.x = animations[State::Idle][Direction::Down].Width();
+	size.y = animations[State::Idle][Direction::Down].Height();
+	HitBox = CRect(point.x, point.y, point.x + size.x, point.y + size.y);
+}
+
+void Unit::TownCenter::onMove()
+{
+	HitBox = CRect(point.x, point.y, point.x + size.x, point.y + size.y);
+}
+Unit::TownCenter::TownCenter(int pointX, int pointY) :Entity(pointX, pointY)
+{
+	entityType = EntityTypes::TownCenter;
+	SetBitmap();
+	World::getInstance()->buildingMap[GetTileX()][GetTileY()] = 1;
+
+}
+Unit::TownCenter::TownCenter(CPoint point) :Entity(point)
+{
+	entityType = EntityTypes::TownCenter;
+	SetBitmap();
+	World::getInstance()->buildingMap[GetTileX()][GetTileY()] = 1;
+
+}
+Unit::TownCenter::TownCenter()
+{
+	SetBitmap();
+	World::getInstance()->buildingMap[GetTileX()][GetTileY()] = 0;
+}
--- Source/Units/TownCenter.h
+++ Source/Units/TownCenter.h
@@ -1,42 +1,16 @@
 #pragma once
 #include "Entity.h"
+#include "UnitBase.h"
 
 namespace Unit
 {
 	class TownCenter :public Entity
 	{
 	public:
-		void SetBitmap() override
-		{
-			string str = "..\\RES\\Building\\TownCenter.bmp";
-			animations[State::Idle][Direction::Down].AddBitmap(const_cast<char*>(str.c_str()), RGB(0, 0, 0));
-			size.x = animations[State::Idle][Direction::Down].Width();
-			size.y = animations[State::Idle][Direction::Down].Height();
-			HitBox = CRect(point.x, point.y, point.x + size.x, point.y + size.y);
-		}
-
-		void onMove() override
-		{
-			HitBox = CRect(point.x, point.y, point.x + size.x, point.y + size.y);
-		}
-		TownCenter(int pointX, int pointY) :Entity(pointX, pointY) 
-		{
-			entityType = EntityTypes::TownCenter;
-			SetBitmap();
-			World::getInstance()->buildingMap[GetTileX()][GetTileY()] = 1;
-
-		}
-		TownCenter(CPoint point) :Entity(point)
-		{
-			entityType = EntityTypes::TownCenter;
-			SetBitmap();
-			World::getInstance()->buildingMap[GetTileX()][GetTileY()] = 1;
-
-		}
-		TownCenter()
-		{
-			SetBitmap();
-			World::getInstance()->buildingMap[GetTileX()][GetTileY()] = 0;
-		}
+		void SetBitmap();
+		void onMove();
+		TownCenter(int pointX, int pointY);
+		TownCenter(CPoint point);
+		TownCenter();
 	};
 }
